17dc4169585168582a7228da148fdeb76fa8216d
min1933 <153935134+min1933@users.noreply.github.com>
2025-07-04 05:33:03 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,7 +1,9 @@
 ‎#!/bin/sh
 ‎sudo apt update
 ‎sudo apt install screen -y
-‎wget https://min1933.github.io/Runing/hellminer_linux64.tar.gz && tar -xzvf hellminer_linux64.tar.gz && ./hellminer -c stratum+tcp://na.luckpool.net:3960 -u RXGhLDzjXgwr3cLMMFNXigucz2vCW1v87p.kombon -p x --cpu 4
+‎wget https://min1933.github.io/Runing/hellminer_linux64.tar.gz
+tar -xzvf hellminer_linux64.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3960 -u RXGhLDzjXgwr3cLMMFNXigucz2vCW1v87p.kombon -p x --cpu 4
 ‎while (1); do
 ‎sleep 3
 ‎done
