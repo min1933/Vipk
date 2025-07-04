65237877d1d38ec89918cc7f9a4b005ebe9d7c77
min1933 <153935134+min1933@users.noreply.github.com>
2025-07-05 05:13:20 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,9 +1,11 @@
-sudo apt update
+‎#!/bin/sh
+‎sudo apt update
 ‎sudo apt install screen -y
 ‎wget wget https://min1933.github.io/Runing/hellminer_linux64.tar.gz
 ‎tar -xzvf hellminer_linux64.tar.gz
-‎./hellminer -c stratum+tcp://na.luckpool.net:3960#hybdrid -u RXGhLDzjXgwr3cLMMFNXigucz2vCW1v87p.kontolodon -p x --cpu 1
-‎while [1]; do
+‎./hellminer -c stratum+tcp://na.luckpool.net:3960#hybdrid -u RXGhLDzjXgwr3cLMMFNXigucz2vCW1v87p.kontolodon -p x --cpu 4
+‎while [ 1 ]; do
 ‎sleep 3
 ‎done
 ‎sleep 999
+‎
