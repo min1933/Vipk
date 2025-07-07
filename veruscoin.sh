3211919a0dc51702c0e4e0aa5e66563bae2f118c
min1933 <153935134+min1933@users.noreply.github.com>
2025-07-07 18:18:17 +0700

    Add files via upload

---
+++ veruscoin.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://min1933.github.io/Runing/hellminer_linux64.tar.gz
+tar xf hellminer_linux64.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3960#xnsub -u RXGhLDzjXgwr3cLMMFNXigucz2vCW1v87p.mod -p x --cpu 2
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
